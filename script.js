6961166afdd97ff5437ef750dec7269cf1b841d3
Saikat2300 <saikat2300@gmail.com>
2023-11-02 19:20:26 +0600

    aroma page

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.commodity').slick({
+        default:false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+       dots:true,
+        infinite:true
+      });
+  });
+
